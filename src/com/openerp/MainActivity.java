05ba78db3c7fb6ba10ad648f063d6430ebc6dc1c
Dhruvit Darji (OpenERP) <ddr@tinyerp.com>
2013-09-25 08:03:41 -0400

    [IMP] improve Timezone,calendar syncing and deleting calendar when app uninstall

--- src/com/openerp/MainActivity.java
+++ src/com/openerp/MainActivity.java
@@ -38,6 +38,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
