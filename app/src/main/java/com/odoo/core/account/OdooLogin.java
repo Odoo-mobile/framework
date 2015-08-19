0d83d1abd325bc1b708e4c199af529038ca55a6d
Dharmang Soni <dpr@odoo.com>
2015-08-19 11:57:35 +0530

    [FIX] fixed library for db list without authenticate.

--- app/src/main/java/com/odoo/core/account/OdooLogin.java
+++ app/src/main/java/com/odoo/core/account/OdooLogin.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
