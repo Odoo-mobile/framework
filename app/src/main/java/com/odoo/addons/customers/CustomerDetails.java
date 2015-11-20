4c2d83803734071f8a75db89e53179cc7dcb3a05
Dharmang Soni (Odoo) <dpr@odoo.com>
2015-11-20 18:49:52 +0530

    [IMP] updated library for odoo9.0+ support [FIX] fixed live search issue [FIX] fixed selection field value casting issue

--- app/src/main/java/com/odoo/addons/customers/CustomerDetails.java
+++ app/src/main/java/com/odoo/addons/customers/CustomerDetails.java
@@ -1,20 +1,20 @@
 /**
  * Odoo, Open Source Management Solution
  * Copyright (C) 2012-today Odoo SA (<http:www.odoo.com>)
- *
+ * <p/>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details
- *
+ * <p/>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http:www.gnu.org/licenses/>
- *
+ * <p/>
  * Created on 8/1/15 5:47 PM
  */
 package com.odoo.addons.customers;
@@ -53,7 +53,7 @@ import com.odoo.widgets.parallax.ParallaxScrollView;
 import odoo.controls.OField;
 import odoo.controls.OForm;
 import odoo.helper.OdooFields;
-import odoo.helper.utils.gson.OdooRecord;
+import odoo.helper.utils.gson.OdooResult;
 
 public class CustomerDetails extends AppCompatActivity
         implements View.OnClickListener, OField.IOnFieldValueChangeListener {
@@ -296,8 +296,8 @@ public class CustomerDetails extends AppCompatActivity
                 Thread.sleep(300);
                 OdooFields fields = new OdooFields();
                 fields.addAll(new String[]{"image_medium"});
-                OdooRecord record = resPartner.getServerDataHelper().read(null, params[0]);
-                if (!record.getString("image_medium").equals("false")) {
+                OdooResult record = resPartner.getServerDataHelper().read(null, params[0]);
+                if (record != null && !record.getString("image_medium").equals("false")) {
                     image = record.getString("image_medium");
                 }
             } catch (Exception e) {
