59102cd93c959adecc1eea869dab322435ee49c2
Dharmang Soni <dpr@odoo.com>
2016-01-25 15:14:27 +0530

    Added JSONUtils.java
    Added removed JSONUtil java file. (commit : https://github.com/Odoo-mobile/framework/commit/224cbbeb6e8b07081d7771f14ee7e95c29766dc2#diff-6e21408d614989df164e0d1e9b27b509)

---
+++ app/src/main/java/com/odoo/core/utils/JSONUtils.java
@@ -0,0 +1,77 @@
+package com.odoo.core.utils;
+
+import android.text.TextUtils;
+
+import com.odoo.core.orm.ODataRow;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class JSONUtils {
+    public static <T> List<T> toList(JSONArray array) {
+        List<T> list = new ArrayList<T>();
+        try {
+            if (array != null) {
+                for (int i = 0; i < array.length(); i++) {
+                    list.add((T) array.get(i));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static <T> JSONArray toArray(List<T> list) {
+        JSONArray array = new JSONArray();
+        try {
+            for (T obj : list)
+                array.put(obj);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return array;
+    }
+
+    public static <T> List<T> toList(String list_data) {
+        List<T> list = new ArrayList<>();
+        try {
+            list.addAll(JSONUtils.<T>toList(new JSONArray(list_data)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static ODataRow toDataRow(JSONObject json) {
+        ODataRow row = new ODataRow();
+        try {
+            @SuppressWarnings("unchecked")
+            Iterator<String> keys = json.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                row.put(key, json.get(key));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return row;
+    }
+
+    public static JSONObject toJSONObject(ODataRow row) {
+        JSONObject json = new JSONObject();
+        try {
+            for (String key : row.keys()) {
+                json.put(key, row.get(key));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+}
